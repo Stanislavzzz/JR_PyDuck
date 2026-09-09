8f266219561244faff3c1e51a4635cfda0e880f4
Stanislav <stanislav3@gmail.com>
2026-09-09 17:36:32 +0300

    Add lesson.02

---
+++ m1/lesson.02/main.py
@@ -0,0 +1,39 @@
+# age = 32
+# print(age)
+# print(type(age))
+# print(id(age))
+#
+# age = "one"
+# print(age)
+# print(type(age))
+# print(id(age))
+#
+# name_1
+# name2
+# name_user
+#
+# nameUser
+
+# _user = "Bob"
+
+# _ = "Bob"
+# print(_)
+
+
+# user = "Bob"
+# # print(user)
+#
+#
+# user = "Anna"
+#
+# print(user)
+
+
+# USER = "Bob"
+# print(USER)
+#
+# USER = "Anna"
+# print(USER)
+#
+
+import this
